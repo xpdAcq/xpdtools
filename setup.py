6824b80c36d24241b480b86ad4615df3d88605d2
christopher <cjwright4242@gmail.com>
2017-12-27 16:04:46 -0500

    WIP: work on tests

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='xpdtools',
-    version='0.2.0',
+    version='0.0.0',
     packages=find_packages(),
     description='data processing module',
     zip_safe=False,
-    package_data={'xpdan': ['config/*']},
+    # package_data={'xpdan': ['config/*']},
     include_package_data=True,
-    entry_points={'console_scripts': 'iq = xpdtools.raw_to_iq:main2'}
+    # entry_points={'console_scripts': 'iq = xpdtools.raw_to_iq:main2'}
 )
--- xpdtools/tools.py
+++ xpdtools/tools.py
@@ -27,7 +27,7 @@ from skbeam.core.mask import margin, binned_outlier
 
 
 def mask_ring_median(a):
-    """Find outlier pixels in a single ring via a single pass with the median """
+    """Find outlier pixels in a single ring via a single pass with the median."""
     values_array, positions_array, alpha = a
     z = np.abs(values_array - np.median(values_array)) / np.std(values_array)
     removals = positions_array[z > alpha]
@@ -43,8 +43,9 @@ def mask_ring_mean(a):
     while len(values_array) > 0:
         m[np.in1d(positions_array, removals)] = False
         v = values_array[m]
-        norm_v_list = np.abs(v - np.mean(v)) / np.std(v)
-        if np.all(norm_v_list < alpha):
+        std = np.std(v)
+        norm_v_list = np.abs(v - np.mean(v)) / std
+        if np.all(norm_v_list < alpha) or std == 0.0:
             break
         # get the index of the worst pixel
         worst_idx = np.argmax(norm_v_list)
@@ -61,14 +62,17 @@ def new_masking_method(img, geo, alpha=3, tmsk=None, mask_method='median'):
 
     Parameters
     ----------
-    img: np.ndarray
+    img : np.ndarray
         The image
-    geo: pyFAI.geometry.Geometry instance
+    geo : pyFAI.geometry.Geometry instance
         The detector geometry information
-    alpha: float
+    alpha : float
         The number of standard deviations to clip
-    tmsk: np.ndarray, optional
+    tmsk : np.ndarray, optional
         Prior mask. If None don't use a prior mask, defaults to None.
+    mask_method : {'median', 'mean'}, optional
+        The method to use for creating the mask, median is faster, mean is more
+        accurate. Defaults to median.
 
     Returns
     -------
@@ -99,96 +103,6 @@ def new_masking_method(img, geo, alpha=3, tmsk=None, mask_method='median'):
     tmsk = tmsk.reshape(img.shape)
     print('finished mask')
     return tmsk.astype(bool)
-
-
-def old_mask_img(img, geo,
-                 edge=30,
-                 lower_thresh=0.0,
-                 upper_thresh=None,
-                 bs_width=13, tri_offset=13, v_asym=0,
-                 alpha=2.5,
-                 tmsk=None):
-    """
-    Mask an image based off of various methods
-
-    Parameters
-    ----------
-    img: np.ndarray
-        The image to be masked
-    geo: pyFAI.geometry.Geometry
-        The pyFAI description of the detector orientation or any
-        subclass of pyFAI.geometry.Geometry class
-    edge: int, optional
-        The number of edge pixels to mask. Defaults to 30. If None, no edge
-        mask is applied
-    lower_thresh: float, optional
-        Pixels with values less than or equal to this threshold will be masked.
-        Defaults to 0.0. If None, no lower threshold mask is applied
-    upper_thresh: float, optional
-        Pixels with values greater than or equal to this threshold will be
-        masked.
-        Defaults to None. If None, no upper threshold mask is applied.
-    bs_width: int, optional
-        The width of the beamstop in pixels. Defaults to 13.
-        If None, no beamstop polygon mask is applied.
-    tri_offset: int, optional
-        The triangular pixel offset to create a pointed beamstop polygon mask.
-        Defaults to 13. If None, no beamstop polygon mask is applied.
-    v_asym: int, optional
-        The vertical asymmetry of the polygon beamstop mask. Defaults to 0.
-        If None, no beamstop polygon mask is applied.
-    alpha: float or tuple or, 1darray, optional
-        Then number of acceptable standard deviations, if tuple then we use
-        a linear distribution of alphas from alpha[0] to alpha[1], if array
-        then we just use that as the distribution of alphas. Defaults to 2.5.
-        If None, no outlier masking applied.
-    tmsk: np.ndarray, optional
-        The starting mask to be compounded on. Defaults to None. If None mask
-        generated from scratch.
-
-    Returns
-    -------
-    tmsk: np.ndarray
-        The mask as a boolean array. True pixels are good pixels, False pixels
-        are masked out.
-
-    """
-    q = geo.qArray(img.shape)
-    qbinned = generate_binner(geo, img.shape)
-    if tmsk is None:
-        working_mask = np.ones(img.shape).astype(bool)
-    else:
-        working_mask = tmsk.copy()
-    if edge:
-        working_mask *= margin(img.shape, edge)
-    if lower_thresh:
-        working_mask *= (img >= lower_thresh).astype(bool)
-    if upper_thresh:
-        working_mask *= (img <= upper_thresh).astype(bool)
-    if all([a is not None for a in [bs_width, tri_offset, v_asym]]):
-        center_x, center_y = [geo.getFit2D()[k] for k in
-                              ['centerX', 'centerY']]
-        nx, ny = img.shape
-        mask_verts = [(center_x - bs_width, center_y),
-                      (center_x, center_y - tri_offset),
-                      (center_x + bs_width, center_y),
-                      (center_x + bs_width + v_asym, ny),
-                      (center_x - bs_width - v_asym, ny)]
-
-        x, y = np.meshgrid(np.arange(nx), np.arange(ny))
-        x, y = x.flatten(), y.flatten()
-
-        points = np.vstack((x, y)).T
-
-        path = Path(mask_verts)
-        grid = path.contains_points(points)
-        # Plug msk_grid into into next (edge-mask) step in automask
-        working_mask *= ~grid.reshape((ny, nx))
-
-    if alpha:
-        working_mask *= binned_outlier(img, q, alpha, qbinned.bin_edges,
-                                       mask=working_mask)
-    return working_mask
 
 
 def mask_img(img, geo,
@@ -283,64 +197,6 @@ def mask_img(img, geo,
     return working_mask
 
 
-def compress_mask(mask):
-    """Compress a mask via a csr sparse matrix
-
-    Parameters
-    ----------
-    mask: 2d boolean array
-        The mask, True/1 are good pixels, False/0 are bad
-
-    Returns
-    -------
-    list:
-        The csr_matrix data
-    list:
-        The csr_matrix indices
-    list:
-        The csr_matrix indptr
-
-    See Also:
-    ---------
-    scipy.sparse.csr_matrix
-    """
-    cmask = csr_matrix(~mask)
-    # FIXME: we may need to also return the mask shape
-    return cmask.data.tolist(), cmask.indices.tolist(), cmask.indptr.tolist()
-
-
-def decompress_mask(data, indices, indptr, shape):
-    """Decompress a csr sparse matrix into a mask
-
-    Parameters
-    ----------
-    data: list
-        The csr_matrix data
-    indices: list
-        The csr_matrix indices
-    indptr: list
-        The csr_matrix indptr
-    shape: tuple
-        The shape of the array to be recreated
-
-    Returns
-    -------
-    mask: 2d boolean array
-        The mask, True/1 are good pixels, False/0 are bad
-
-    See Also:
-    ---------
-    scipy.sparse.csr_matrix
-    """
-    cmask = csr_matrix(
-        tuple([np.asarray(a) for a in [data, indices, indptr]]), shape=shape)
-    return ~cmask.toarray().astype(bool)
-
-
-def pull_array(img2):
-    return img2
-
-
 def generate_binner(geo, img_shape=None, mask=None):
     if img_shape is None:
         img_shape = mask.shape
@@ -351,7 +207,7 @@ def generate_binner(geo, img_shape=None, mask=None):
     pixel_size = [getattr(geo, a) for a in ['pixel1', 'pixel2']]
     rres = np.hypot(*pixel_size)
     rbins = np.arange(np.min(r) - rres / 2., np.max(r) + rres / 2., rres / 2.)
-    # This is only called once, use the function version
+    # This is only called once, use the function version`
     rbinned = BinnedStatistic1D(r.ravel(), statistic=np.max, bins=rbins, )
 
     qbin_sizes = rbinned(q_dq.ravel())
@@ -397,15 +253,6 @@ def load_geo(cal_params):
     ai = AzimuthalIntegrator()
     ai.setPyFAI(**cal_params)
     return ai
-
-
-def event_count(x):
-    return x['count'] + 1
-
-
-def add_img(img1, img2):
-    # Note that this exists because accumulate doesn't take args yet
-    return img1 + img2
 
 
 def pdf_getter(x, y, composition, **kwargs):
