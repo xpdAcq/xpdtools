3e4e8164357258de50e0d71e70536e255db72d90
christopher <cjwright4242@gmail.com>
2017-12-27 19:03:42 -0500

    CI: fix graphviz dep

--- xpdtools/pipelines/raw_pipeline.py
+++ xpdtools/pipelines/raw_pipeline.py
@@ -11,7 +11,7 @@ mask_kwargs = {}
 fq_kwargs = dict(dataformat='QA', qmaxinst=28, qmax=25, rstep=np.pi / 25)
 pdf_kwargs = dict(dataformat='QA', qmaxinst=28, qmax=22, rstep=np.pi / 22)
 
-# Detector corrections {
+# Detector corrections
 raw_foreground = Stream(stream_name='raw foreground')
 raw_foreground_dark = Stream(stream_name='raw foreground dark')
 raw_background = Stream(stream_name='raw background')
@@ -29,9 +29,8 @@ bg_corrected_img = (
         zip_latest(dark_corrected_background).
         map(op.sub, stream_name='background corrected img')
 )
-# }
 
-# Calibration management {
+# Calibration management
 wavelength = Stream(stream_name='wavelength')
 calibrant = Stream(stream_name='calibrant')
 detector = Stream(stream_name='detector')
@@ -55,9 +54,8 @@ geometry = (geo_input.zip_latest(is_calibration_img).
     pluck(0, stream_name='Gate calibration').
     map(load_geo).
     union(gen_geo, stream_name='Combine gen and load cal'))
-# }
 
-# Image corrections {
+# Image corrections
 pol_corrected_img = (bg_corrected_img.
     zip_latest(geometry).
     map(lambda a, **kwargs: polarization_correction(
@@ -66,10 +64,8 @@ pol_corrected_img = (bg_corrected_img.
 mask = (pol_corrected_img.
     zip_latest(geometry).
     map(mask_img, stream_name='mask', **mask_kwargs))
-# }
 
-# Integration {
-# TODO: seperate q/tth from statistics
+# Integration
 binner = (mask.zip_latest(pol_corrected_img, geometry).map(generate_binner))
 img_binner = pol_corrected_img.zip_latest(binner)
 mean = img_binner.starmap(lambda img, binner, **kwargs: binner(img, **kwargs),
@@ -83,13 +79,12 @@ q = binner.map(lambda x: x.bin_centers)
 tth = q.zip_latest(wavelength).starmap(q_to_twotheta, stream_name='tth')
 
 z_score = img_binner.map(z_score_image, stream_name='z score')
-# }
 
-# PDF {
+# PDF
 composition = Stream(stream_name='composition')
 iq_comp = q.zip(mean).zip_latest(composition)
 fq = iq_comp.starmap(fq_getter, stream_name='fq', **fq_kwargs)
 pdf = iq_comp.starmap(pdf_getter, stream_name='pdf', **pdf_kwargs)
-# }
-raw_foreground.visualize('/home/christopher/raw_mystream.png',
-                         source_node=True)
+
+# raw_foreground.visualize('/home/christopher/raw_mystream.png',
+#                          source_node=True)
