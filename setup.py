f903d62e516ce2bc2c577fddcb0916abc7c0ff73
christopher <cjwright4242@gmail.com>
2018-04-23 15:33:00 -0400

    bumped version to 0.1.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xpdtools',
-    version='0.1.8',
+    version='0.1.9',
     packages=find_packages(),
     description='data processing module',
     zip_safe=False,
