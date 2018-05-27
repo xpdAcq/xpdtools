138d9290e9567f2ab9a4acf9bad9815629888a3c
christopher <cjwright4242@gmail.com>
2018-05-27 16:06:27 -0400

    FIX: fix the graph mangling

--- xpdtools/pipelines/qoi.py
+++ xpdtools/pipelines/qoi.py
@@ -23,9 +23,9 @@ r_peak_pos = (pdf_peaks
               .starmap(lambda x, y: y[x]))
 
 # Hack to properly order the pipeline
-for s in [mean, true_pdf]:
+for s, t in zip([mean, true_pdf], [mean_peaks, pdf_peaks]):
     for n in s.downstreams.data:
-        if n() is pdf_peaks.upstreams[0]:
+        if n() is t.upstreams[0]:
             break
     s.downstreams.data._od.move_to_end(n, last=True)
     del n
