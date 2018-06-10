f36528fda988587497d6e7a69b8af221db288d38
christopher <cjwright4242@gmail.com>
2018-06-10 17:01:13 -0400

    ENH: pipeline for flat field experiments

--- xpdtools/pipelines/flatfield.py
+++ xpdtools/pipelines/flatfield.py
@@ -2,19 +2,11 @@
 import operator as op
 
 import numpy as np
-from skbeam.core.utils import q_to_twotheta
 from streamz_ext import Stream
 
 from xpdtools.calib import img_calibration
 from xpdtools.tools import (load_geo, mask_img, map_to_binner,
-                            fq_getter, pdf_getter, sq_getter,
                             generate_map_bin, move_center)
-
-
-def fprint(x):
-    print(x)
-    return x
-
 
 mask_setting = {'setting': 'auto'}
 
@@ -26,19 +18,19 @@ raw_background_dark = Stream(stream_name='raw background dark')
 
 # Get the image shape for the binner
 dark_corrected_foreground = (
-    raw_foreground.
-        combine_latest(raw_foreground_dark, emit_on=0).
-        starmap(op.sub)
+    raw_foreground
+    .combine_latest(raw_foreground_dark, emit_on=0)
+    .starmap(op.sub)
 )
 dark_corrected_background = (
-    raw_background.
-        combine_latest(raw_background_dark, emit_on=0).
-        starmap(op.sub)
+    raw_background
+    .combine_latest(raw_background_dark, emit_on=0)
+    .starmap(op.sub)
 )
 bg_corrected_img = (
-    dark_corrected_foreground.
-        combine_latest(dark_corrected_background, emit_on=0).
-        starmap(op.sub, stream_name='background corrected img')
+    dark_corrected_foreground
+    .combine_latest(dark_corrected_background, emit_on=0)
+    .starmap(op.sub, stream_name='background corrected img')
 )
 
 # Calibration management
@@ -54,22 +46,23 @@ gated_cal = (
         pluck(0, stream_name='Gate calibration'))
 
 gen_geo_cal = (
-    gated_cal.
-        combine_latest(wavelength,
-                       calibrant,
-                       detector, emit_on=0).
-        starmap(img_calibration)
+    gated_cal
+    .combine_latest(wavelength,
+                    calibrant,
+                    detector, emit_on=0)
+    .starmap(img_calibration)
 )
 
 gen_geo = gen_geo_cal.pluck(1)
 
 pre_geometry = (
     geo_input
-        .combine_latest(is_calibration_img, emit_on=0)
-        .filter(lambda a: not bool(a[1]))
-        .pluck(0, stream_name='Gate calibration')
-        .map(load_geo)
-        .union(gen_geo, stream_name='Combine gen and load cal'))
+    .combine_latest(is_calibration_img, emit_on=0)
+    .filter(lambda a: not bool(a[1]))
+    .pluck(0, stream_name='Gate calibration')
+    .map(load_geo)
+    .union(gen_geo, stream_name='Combine gen and load cal')
+)
 # XXX: This is a critical assumption in the workflow we need to make sure
 # that the PONI of a calibration can be moved by just moving the detector
 geometry = motors.combine_latest(pre_geometry, emit_on=0).starmap(move_center)
@@ -148,16 +141,16 @@ f_img_binner = pol_corrected_img.map(np.ravel).combine_latest(binner,
                                                               emit_on=0)
 
 ave = (
-    f_img_binner.
-        starmap(lambda img, binner, **kwargs: binner(img, **kwargs),
-                statistic='mean', stream_name='Mean IQ')
-    # .map(np.nan_to_num)
+    f_img_binner
+    .starmap(lambda img, binner, **kwargs: binner(img, **kwargs),
+             statistic='mean', stream_name='Mean IQ')
 )
 
 mean_array = (ave
               .map(lambda x: np.hstack((np.ones(1) * np.nan, x,
                                         np.ones(1) * np.nan)))
-              .combine_latest(img_shape, bins, emit_on=0))
+              .combine_latest(img_shape, bins, emit_on=0)
+              .starmap(lambda x, y, z: np.ones(y) * x[z]))
 
 calc_ff = pol_corrected_img.zip(mean_array).starmap(op.truediv)
 total_ff = calc_ff.accumulate(lambda state, x: state + x)
