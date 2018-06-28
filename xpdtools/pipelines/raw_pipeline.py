a4e9233ccf4674b6a3a538bd1cef63800992ec9e
christopher <cjwright4242@gmail.com>
2018-06-28 16:34:46 -0400

    flake8

--- xpdtools/pipelines/raw_pipeline.py
+++ xpdtools/pipelines/raw_pipeline.py
@@ -4,7 +4,10 @@ from streamz_ext.link import link
 from xpdtools.pipelines.calibration import make_pipeline as cal_mp
 from xpdtools.pipelines.image_preprocess import make_pipeline as ipmp
 from xpdtools.pipelines.integration import make_pipeline as bp
-from xpdtools.pipelines.masking import make_pipeline as mp, mask_setting  # noqa: F401
+from xpdtools.pipelines.masking import (  # noqa: F401
+    make_pipeline as mp,
+    mask_setting,
+)
 from xpdtools.pipelines.pdf import make_pipeline as pdfp
 from xpdtools.pipelines.scattering_correction import make_pipeline as scat_cor
 
