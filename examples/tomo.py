3b9d18a37732bb6c9df4a079f9a18329f950315d
christopher <cjwright4242@gmail.com>
2018-12-04 10:25:44 -0500

    piecewise recon

--- examples/tomo.py
+++ examples/tomo.py
@@ -7,7 +7,7 @@ from rapidz import Stream
 from bluesky.callbacks.broker import LiveImage
 import matplotlib.pyplot as plt
 from bluesky.utils import install_qt_kicker
-from xpdtools.pipelines.tomo import tomo_pipeline_theta
+from xpdtools.pipelines.tomo import tomo_prep, tomo_pipeline_piecewise
 
 
 fname = os.path.expanduser('~/Downloads/tooth.h5')
@@ -21,47 +21,73 @@ rot_center = tomopy.find_center(proj, theta, init=290, ind=0, tol=0.5)
 
 
 backend = 'thread'
+# from dask.distributed import Client
+# c = Client()
 
 center = Stream(stream_name='center')
 proj_node = Stream(stream_name='projection')
 theta_node = Stream(stream_name='theta')
 
-# z.sink(lambda x: print(x[0].shape))
 install_qt_kicker()
 
 li = LiveImage('hi', cmap='viridis')
-ns = tomo_pipeline_theta((proj_node
-                          # .scatter(backend=backend)
-                          ),
-                         (theta_node
-                          # .scatter(backend=backend)
-                          ),
-                    center=(center
-                            # .scatter(backend=backend)
-                            ))
-z = ns['tomo_node']
-zz = (z
-      # .buffer(1000).gather()
-      .pluck(0))
-# zz.sink(print)
-zz.sink(li.update)
-#proj_node.visualize(
-#     source_node=True,
-    # rankdir='LR',
-    # ratio='.1',
-    # labelfontsize='20',
-    # nodesep='.1', ranksep='.1',
-    # dpi='300'
-#)
+# li2 = LiveImage('hi', cmap='viridis')
 
-plt.pause(.1)
+x = Stream()
+th = Stream()
+th_dim = Stream()
+x_dim = Stream()
+th_extents = Stream()
+x_extents = Stream()
+qoi = Stream()
+
+
+ns = tomo_prep(x.scatter(backend=backend),
+               th.scatter(backend=backend),
+               th_dim.scatter(backend=backend),
+               x_dim.scatter(backend=backend),
+               th_extents.scatter(backend=backend),
+               x_extents.scatter(backend=backend))
+# ns = tomo_prep(x,
+#                th,
+#                th_dim,
+#                x_dim,
+#                th_extents,
+#                x_extents)
+ns2 = tomo_pipeline_piecewise(qoi.scatter(backend=backend),
+                              center=center.scatter(backend=backend), **ns)
+# ns2 = tomo_pipeline_piecewise(qoi,
+#                               center=center,
+#                               **ns)
+ns.update(**ns2)
+z = ns['rec']
+zz = (z
+      .buffer(1000).gather()
+      .pluck(0)
+      )
+zz.sink(li.update)
+# (ns['sineogram']
+#  .map(lambda x: x[:, 0, :])
+#  .buffer(1000).gather()
+#  .sink(li2.update))
+
+# plt.pause(.1)
+# qoi.visualize()
 
 center.emit(rot_center)
-input()
-for pr, th in zip(proj, theta):
-    proj_node.emit(pr)
-    theta_node.emit(th)
-    plt.pause(.01)
+th_dim.emit(len(theta))
+x_dim.emit(proj.shape[-1])
+th_extents.emit([0, 180])
+x_extents.emit([0, proj.shape[-1]])
+# ns['sineogram'].emit(proj)
+# input()
+for i in range(100, 110):
+    for j in range(0, len(theta)):
+        x.emit(i)
+        th.emit(j)
+        qoi.emit(proj[j, 0, i])
+        print(i, j)
+#     plt.pause(1)
 print('done')
 
 plt.show()
--- xpdtools/pipelines/tomo.py
+++ xpdtools/pipelines/tomo.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tomopy
+import operator as op
 
 
 def append_data(acc, pt):
@@ -19,19 +20,17 @@ def reshape(x):
 
 
 def min_pos(x, y):
-    return np.argmin((x - y) **2)
+    return np.argmin((x - y) ** 2)
 
 
 def tomo_pipeline_theta(qoi, theta, center, algorithm="gridrec"):
     tomo_node = (
-        qoi.map(reshape,
-                stream_name='reshape')
+        qoi.map(reshape, stream_name="reshape")
         .map(tomopy.minus_log)
         .zip(theta)
         .accumulate(append_data)
         .combine_latest(center, emit_on=0)
-        .map(flatten
-    )
+        .map(flatten)
         .starmap(tomopy.recon, algorithm=algorithm)
     )
     return locals()
@@ -50,21 +49,56 @@ def tomo_pipeline_x(qoi, y, center, algorithm="gridrec"):
     return locals()
 
 
-def tomo_prep(x, theta, th_dim, x_dim, th_extents, x_extents):
+def tomo_prep(x, th, th_dim, x_dim, th_extents, x_extents, **kwargs):
     """Preperation chunk for tomography, mostly munging positional data
        into array coordinates"""
     # dims -> (91, 44)
     # extents -> ([180, 0], [-8, 9.5])
     x_ext = x_extents.zip(x_dim).map(flatten).starmap(np.linspace)
-    th_ext = th_extents.zip(th_dim).map(flatten).starmap(np.linspace)
+    th_ext = (
+        th_extents.zip(th_dim)
+        .map(flatten)
+        .starmap(np.linspace)
+        .map(np.deg2rad)
+    )
     x_pos = x.combine_latest(x_ext, emit_on=x).starmap(min_pos)
     th_pos = th.combine_latest(th_ext, emit_on=th).starmap(min_pos)
     return locals()
 
 
-def tomo_pipeline_piecewise(qoi, x_pos, th_pos, th_dim, x_dim, center, algorithm='gridrec'):
+# TODO: this might not be ok long term, since many things will want to access
+#  this array and that might not be stable, maybe need to make new copies
+#  maybe better as an accumulator which mints new arrays?
+def fill_sinogram(q, xp, thp, esa):
+    esa[xp, 0, thp] = q
+    return esa
+
+
+def tomo_pipeline_piecewise(
+    qoi,
+    x_pos,
+    th_pos,
+    th_dim,
+    x_dim,
+    center,
+    th_ext,
+    algorithm="gridrec",
+    **kwargs
+):
     """Perform a tomographic reconstruction on a QOI"""
-    empty_sineogram_array = th_dim.zip(x_dim).starmap(lambda a, b: np.ones(a, 1, b)
-    sineogram = qoi.zip(x_pos, th_pos).combine_latest(empty_sineogram_array, emit_on=0).map(lambda a, b: *a, b).starmap(lambda q, xp, thp, esa: esa[xp, 1, thp]=q)
-    rec = sineogram.combine_latest(th_ext, center, emit_on=sineogram).starmap(tomopy.recon, algorithm=algorithm)
+    empty_sineogram_array = th_dim.zip(x_dim).starmap(
+        lambda a, b: np.ones((a, 1, b))
+    )
+    sineogram = (
+        qoi.zip(th_pos, x_pos)
+        .combine_latest(empty_sineogram_array, emit_on=0)
+        .map(flatten)
+        .starmap(fill_sinogram)
+    )
+
+    rec = (
+        sineogram.map(tomopy.minus_log)
+        .combine_latest(th_ext, center, emit_on=0)
+        .starmap(tomopy.recon, algorithm=algorithm)
+    )
     return locals()
