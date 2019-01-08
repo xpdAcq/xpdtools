cf4f3e5c76587f1d16cb8cda76999176f6eabda0
christopher <cjwright4242@gmail.com>
2019-01-08 15:49:23 -0500

    WIP: fix the tomo pipeline (pull out nans)

--- benchmarks/pipeline.py
+++ benchmarks/pipeline.py
@@ -6,8 +6,13 @@ from xpdtools.pipelines.raw_pipeline import *
 
 # from xpdtools.pipelines.extra import *
 from profilehooks import profile
+from rapidz.link import link
 
-mask_setting.update(setting="first")
+namespace['mask_setting'].update(setting="first")
+
+namespace = link(*pipeline_order, **namespace)
+
+
 
 # dark_corrected_background.sink(print)
 # pol_corrected_img_zip.sink(print)
@@ -18,23 +23,25 @@ mask_setting.update(setting="first")
 geo = pyFAI.load("test.poni")
 img = imread("test.tiff")
 
-geometry.emit(geo)
-composition.emit("Au")
-for n in [raw_background_dark, raw_background, raw_foreground_dark]:
+namespace['geometry'].emit(geo)
+namespace['composition'].emit("Au")
+for n in [namespace['raw_background_dark'],
+          namespace['raw_background'],
+          namespace['raw_foreground_dark']]:
     n.emit(np.zeros(img.shape))
 
 
 @profile(
-    skip=1,
-    # sort='module',
+    skip=1000,
+    sort='tottime',
     entries=20,
 )
 def f(i):
-    img_counter.emit(i)
-    raw_foreground.emit(img)
+    namespace['img_counter'].emit(i)
+    namespace['raw_foreground'].emit(img)
 
 
-for i in range(11):
+for i in range(5000):
     f(i + 1)
 
 # all masked 5.358 11 calls skip first
