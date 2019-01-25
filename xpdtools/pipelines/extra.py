b066a2be7ef371318051129a5ff49bd2a963503e
Christopher J. Wright <cjwright4242@gmail.com>
2019-01-24 22:29:59 -0500

    combine with mean

--- xpdtools/pipelines/extra.py
+++ xpdtools/pipelines/extra.py
@@ -14,7 +14,7 @@ def median_gen(f_img_binner, **kwargs):
 def std_gen(f_img_binner, mean, **kwargs):
     std = f_img_binner.starmap(
         call_stream_element, statistic="std", stream_name="Mean IQ"
-    ).starmap(op.truediv).map(np.nan_to_num)
+    ).combine_latest(mean, emit_on=0).starmap(op.truediv).map(np.nan_to_num)
     return locals()
 
 
