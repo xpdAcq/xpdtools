94910e73ae5f42f595f68a748eb9eccff2481cb3
Christopher J. Wright <cjwright4242@gmail.com>
2019-03-05 11:26:29 -0500

    black

--- xpdtools/tests/test_tomo_tools.py
+++ xpdtools/tests/test_tomo_tools.py
@@ -5,17 +5,16 @@ import pytest
 from xpdtools.pipelines.tomo import recon_wrapper
 
 
-@pytest.mark.parametrize('n', [2, 3, 4])
+@pytest.mark.parametrize("n", [2, 3, 4])
 def test_recon_wrapper(n):
-    projection = np.random.random([10]*n)
+    projection = np.random.random([10] * n)
     theta = np.linspace(0, 180, 10)
     center = projection.shape[-1] / 2
 
-    res = recon_wrapper(projection, theta, center,
-                        algorithm='gridrec')
+    res = recon_wrapper(projection, theta, center, algorithm="gridrec")
     assert res is not None
     assert res.shape == projection.shape
     if len(projection.shape) == 3:
-        assert_array_almost_equal(res, tomopy.recon(
-            projection, theta, center, algorithm='gridrec'))
-
+        assert_array_almost_equal(
+            res, tomopy.recon(projection, theta, center, algorithm="gridrec")
+        )
