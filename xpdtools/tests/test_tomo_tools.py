1d5c84f99c5dc9644efa6d698eaefee10485e241
Christopher J. Wright <cjwright4242@gmail.com>
2019-03-13 11:07:36 -0400

    test 2D stack

--- xpdtools/tests/test_tomo_tools.py
+++ xpdtools/tests/test_tomo_tools.py
@@ -2,7 +2,8 @@ import numpy as np
 import tomopy
 from numpy.testing import assert_array_almost_equal
 import pytest
-from xpdtools.pipelines.tomo import recon_wrapper
+from rapidz import Stream
+from xpdtools.pipelines.tomo import recon_wrapper, tomo_stack_2D
 
 
 @pytest.mark.parametrize("n", [2, 3, 4])
@@ -18,3 +19,23 @@ def test_recon_wrapper(n):
         assert_array_almost_equal(
             res, tomopy.recon(projection, theta, center, algorithm="gridrec")
         )
+
+
+def test_tomo_stack_2D():
+    rec = Stream()
+    stack_position = Stream()
+    start = Stream()
+    ns = tomo_stack_2D(rec, stack_position, start)
+    L = ns['rec_3D'].sink_to_list()
+
+    stack_position.emit(0)
+    rec.emit(np.ones((10, 10)))
+    stack_position.emit(0)
+    rec.emit(np.ones((10, 10))*2)
+    assert_array_almost_equal(L[-1], np.ones((10, 10, 1))*2)
+
+    stack_position.emit(1)
+    rec.emit(np.ones((10, 10, 1)) * 3)
+    assert_array_almost_equal(
+        L[-1],
+        np.dstack((np.ones((10, 10, 1)) * 2, np.ones((10, 10, 1))*3)))
