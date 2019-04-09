b8dc4bf749b37b95013a8a4af4ab314668409692
Maddison Brod <mkb2180@columbia.edu>
2019-04-09 10:29:42 -0400

    modified test_ringfinding with decorator so that it tests the findrings() function on 5 Ni tiff images. This version of the test function tests whether or not the x and y pixel coordinates of the center point given by findrings()  are within 10 pixels of that given by paiFAI

--- xpdtools/tests/test_ringfinding.py
+++ xpdtools/tests/test_ringfinding.py
@@ -2,18 +2,28 @@ from xpdtools.calib4 import findrings
 
 import tifffile as tf
 
+import pyFAI
+
 from pkg_resources import resource_filename as rs_fn
 
 import os
+import pytest
 
 DATA_DIR=rs_fn("xpdtools", "data/")
 
-filename="Ni_pin_20181101-075909_973de2_0001.tiff"
+filename=["Ni_onTape_forSlimes_andWahsers_20180812-234250_fe70b9_0001.tiff", 'Ni_calib_20180811-191034_63e554_0001.tiff', 'Ni_pin_20181101-075909_973de2_0001.tiff', 'Ni_calib_20180920-230956_0eedc4_0001.tiff','Ni_calib_20180923-133223_c2a848_0001.tiff']
 
-impath=os.path.join(DATA_DIR, filename)
 
-imarray=tf.imread(impath)
-
-def test_ringfinding():
-	center_pt, pointsimage=findrings(imarray)
-	#assert center_pt==right_center
+@pytest.mark.parametrize("filename,poni", [(filename[0], os.path.splitext(filename[0])[0]+'.edf'),
+	                                 (filename[1], os.path.splitext(filename[1])[0]+'.edf'),
+	                                 (filename[2], os.path.splitext(filename[2])[0]+'.edf'),
+	                                 (filename[3], os.path.splitext(filename[3])[0]+'.edf'),
+	                                 (filename[4], os.path.splitext(filename[4])[0]+'.edf') ])
+def test_ringfinding(filename, poni):
+	impath=os.path.join(DATA_DIR, filename)
+	imarray = tf.imread(impath)
+	pointsimage, center_pt=findrings(imarray)
+	d=pyFAI.load(poni)
+	centerx=d.getFit2D()['centerX']
+	centery=d.getFit2D()['centerY']
+	assert abs(center_pt[1]-centerx)<=10 and abs(center_pt[0]-centery)<=10
